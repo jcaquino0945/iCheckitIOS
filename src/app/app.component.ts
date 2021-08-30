a1a38f3c93a6991454e96a65df2d500a5ed8c400
Hans Nituda <hanspatrick.nituda.iics@ust.edu.ph>
2021-08-30 23:53:18 +0800

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { Theme } from '@nativescript/theme'
 @Component({
   selector: 'ns-app',
   templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   private _activatedUrl: string
