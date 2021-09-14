95a07156c94c572eef163f9ee42b9d84825a35e8
Hans Nituda <hanspatrick.nituda.iics@ust.edu.ph>
2021-09-14 19:52:55 +0800

    file upload

--- src/app/Dashboard/DashboardDetails/DashboardDetails.component.ts
+++ src/app/Dashboard/DashboardDetails/DashboardDetails.component.ts
@@ -1,9 +1,13 @@
-import { Application } from "@nativescript/core";
+import { Application, View } from "@nativescript/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { switchMap } from "rxjs/operators";
 import { firebase, firestore } from "@nativescript/firebase";
+import {
+  Mediafilepicker,
+  FilePickerOptions
+} from "nativescript-mediafilepicker";
 
 @Component({
   selector: "DashboardDetails",
@@ -15,38 +19,44 @@ export class DashboardDetailsComponent implements OnInit {
   userData;
   userDetails;
   taskData;
+  file;
+  private _hostView: View;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id');
+    this.id = this.route.snapshot.paramMap.get("id");
     firebase
-    .getCurrentUser()
-    .then(user => {
-      (this.userData = user),
-        firestore
-          .collection("users")
-          .doc(this.userData.uid)
-          .get().then(doc => {
-            if (doc.exists) {
-              console.log(`Document data: ${JSON.stringify(doc.data())}`);
-              const taskDocument = firestore.collection("tasks").doc(this.route.snapshot.paramMap.get('id'));
+      .getCurrentUser()
+      .then(user => {
+        (this.userData = user),
+          firestore
+            .collection("users")
+            .doc(this.userData.uid)
+            .get()
+            .then(doc => {
+              if (doc.exists) {
+                console.log(`Document data: ${JSON.stringify(doc.data())}`);
+                const taskDocument = firestore
+                  .collection("tasks")
+                  .doc(this.route.snapshot.paramMap.get("id"));
 
-              // note that the options object is optional, but you can use it to specify the source of data ("server", "cache", "default").
-              taskDocument.get({ source: "cache" }).then(doc => {
-                if (doc.exists) {
-                  this.taskData = doc.data();
-                  console.log(`Document data: ${JSON.stringify(doc.data())}`);
-                } else {
-                  console.log("No such document!");
-                }
-              });
-              this.userDetails = doc.data();
-            } else {
-              console.log("No such document!");
-            }
-          });
-    })
-    .catch(error => console.log("Trouble in paradise: " + error));
+                // note that the options object is optional, but you can use it to specify the source of data ("server", "cache", "default").
+                taskDocument.get({ source: "cache" }).then(doc => {
+                  if (doc.exists) {
+                    this.taskData = doc.data();
+                    console.log(`Document data: ${JSON.stringify(doc.data())}`);
+                  } else {
+                    console.log("No such document!");
+                  }
+                });
+                this.userDetails = doc.data();
+              } else {
+                console.log("No such document!");
+              }
+            });
+      })
+      .catch(error => console.log("Trouble in paradise: " + error));
   }
 
   onDrawerButtonTap(): void {
@@ -54,7 +64,57 @@ export class DashboardDetailsComponent implements OnInit {
     sideDrawer.showDrawer();
   }
 
-  submitTask() {
-    alert("Submit attachment of task");
-  }
+  // submitTask() {
+  //   alert("Submit attachment of task");
+  // }
+
+  // submitTask() {
+  //   let extensions = [];
+
+  //   if (Application.ios) {
+  //     extensions = [kUTTypePDF, kUTTypeText]; // you can get more types from here: https://developer.apple.com/documentation/mobilecoreservices/uttype
+  //   } else {
+  //     extensions = ["png", "pdf", "jpg"];
+  //   }
+
+  //   let options: FilePickerOptions = {
+  //     android: {
+  //       extensions: extensions,
+  //       maxNumberFiles: 1
+  //     },
+  //     ios: {
+  //       extensions: extensions,
+  //       multipleSelection: true,
+  //       hostView: this._hostView
+  //     }
+  //   };
+
+  //   let mediafilepicker = new Mediafilepicker();
+
+  //   mediafilepicker.openFilePicker(options);
+
+  //   mediafilepicker.on("getFiles", function(res) {
+  //     let results = res.object.get("results");
+  //     console.dir(results);
+
+  //     if (results) {
+  //       for (let i = 0; i < results.length; i++) {
+  //         let result = results[i];
+  //         console.log(result.file);
+  //         this.file = result.file
+  //         console.log(this.file);
+  //       }
+  //     }
+  //   });
+
+  //   mediafilepicker.on("error", function(res) {
+  //     let msg = res.object.get("msg");
+  //     console.log(msg);
+  //   });
+
+  //   mediafilepicker.on("cancel", function(res) {
+  //     let msg = res.object.get("msg");
+  //     console.log(msg);
+  //   });
+  // }
 }
