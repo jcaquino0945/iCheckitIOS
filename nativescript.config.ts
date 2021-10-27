eade7f4d016db78d00b996ffa21ce212d95a1f54
Devdan <devdan@Toms-MacBook-Pro.local>
2021-10-27 22:23:35 +0800

    cancel submission real time

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.iCheckitIOS',
+  id: 'org.nativescript.iCheckit.IOS',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
