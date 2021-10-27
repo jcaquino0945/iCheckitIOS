3aca8364910c4bbd59461c5b0d2a8cab1165a383
Devdan <devdan@Toms-MacBook-Pro.local>
2021-10-27 10:51:16 +0800

    changed app id

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.iCheckit',
+  id: 'org.nativescript.iCheckitIOS',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
